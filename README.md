2a6d17d5dbe126386812356c53e63ea3acc5a2fc
Alexandru Petrescu <alexpetre913@gmail.com>
2017-02-08 13:54:15 +0800

    action and types clear & ReadMe added.

---
+++ README.md
@@ -0,0 +1,22 @@
+# FreedCamp
+
+## Setup
+
+1. **Clone the repo**
+
+  ```
+  $ git clone https://github.com/alexprodev/Freed_Test.git
+  $ cd Freed_Test
+  ```
+
+2. **Install dependencies** (npm v3+):
+
+  ```
+  $ npm install
+  ```
+
+3. **Running on iOS:**
+
+  ```
+  $ react-native run-ios
+  ```
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,57 +1,11 @@
 /**
- * Copyright 2016 Facebook, Inc.
- *
- * You are hereby granted a non-exclusive, worldwide, royalty-free license to
- * use, copy, modify, and distribute this software in source code or binary
- * form for use in connection with the web services and APIs provided by
- * Facebook.
- *
- * As with any software that integrates with the Facebook platform, your use
- * of this software is subject to the Facebook Developer Principles and
- * Policies [http://developers.facebook.com/policy/]. This copyright notice
- * shall be included in all copies or substantial portions of the software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE
- * @providesModule AppActions
  * @flow
  */
 
 'use strict';
 
 const navigationActions = require('./navigation');
-const welcomeNavigationActions = require('./welcomeNavigation');
-const loginActions = require('./login');
-const menteeActions = require('./mentee');
-const mentorActions = require('./mentor');
-const filterActions = require('./filter');
-const messageActions = require('./messages');
-const filtersActions = require('./filters');
-const termPrivacy = require('./termPrivacy');
-const chatMessages = require('./chatMessages');
-const chatBotMessages = require('./chatBotMessages');
-const chatRooms = require('./chatRooms');
-const profile = require('./profile');
-const userActions = require('./user');
 
 module.exports = {
-  ...loginActions,
-  ...menteeActions,
-  ...mentorActions,
-  ...filterActions,
-  ...navigationActions,
-  ...welcomeNavigationActions,
-  ...messageActions,
-  ...filtersActions,
-  ...termPrivacy,
-  ...chatMessages,
-  ...chatBotMessages,
-  ...chatRooms,
-  ...profile,
-  ...userActions
+  ...navigationActions
 };
